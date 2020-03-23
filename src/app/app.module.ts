62c8430f94c755634f79f88f9d9999e1dcc44ce5
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-03-23 14:10:49 +0200

    made navbar collapse properly on click

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,10 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // NGX-BOOTSTRAP COMPONENTS
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
