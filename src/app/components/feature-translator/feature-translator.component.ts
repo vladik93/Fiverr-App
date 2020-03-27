4ac0a6c26fe38286f05df1bb7508fc71e1ea1c81
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-03-27 18:42:49 +0300

    added unsubscribe method on ngDestroy (navbar translators)

--- src/app/components/feature-translator/feature-translator.component.ts
+++ src/app/components/feature-translator/feature-translator.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Translator } from '../../models/translator';
 import { TranslatorService } from '../../services/translator.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-feature-translator',
   templateUrl: './feature-translator.component.html',
   styleUrls: ['./feature-translator.component.css']
 })
-export class FeatureTranslatorComponent implements OnInit {
+export class FeatureTranslatorComponent implements OnInit, OnDestroy {
 
+  subscription: Subscription;
   translator: Translator;
 
   constructor(private tranService: TranslatorService) { }
 
   ngOnInit() {
-    this.tranService.featureTranslator$
+    this.subscription = this.tranService.featureTranslator$
     .subscribe(
       data => this.translator = data[0]
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
