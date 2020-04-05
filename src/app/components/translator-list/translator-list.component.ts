c977a1ec928ecf56ee2cbcb98be87aecb88b09b1
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-04-05 13:46:38 +0300

    added dynamic accordion profiles with a loop instead of mockup

--- src/app/components/translator-list/translator-list.component.ts
+++ src/app/components/translator-list/translator-list.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class TranslatorListComponent implements OnInit {
   oneAtATime = true;
   customClass = 'customClass';
+
+  translators = [
+    {name: 'Alejandro', image: 'assets/images/translator_pic.jpg', description: 'This is a profile description'},
+    {name: 'Maxim', image: 'assets/images/translator_pic2.jpg', description: 'This is a profile description'},
+    {name: 'Jack McBlack', image: 'assets/images/translator_pic3.jpg', description: 'This is a profile description'},
+    {name: 'Scary Woman', image: 'assets/images/translator_pic4.jpg', description: 'This is a profile description'},
+    {name: 'Alejandro', image: 'assets/images/translator_pic.jpg', description: 'This is a profile description'},
+    {name: 'Maxim', image: 'assets/images/translator_pic2.jpg', description: 'This is a profile description'},
+    {name: 'Jack McBlack', image: 'assets/images/translator_pic3.jpg', description: 'This is a profile description'},
+    {name: 'Alejandro', image: 'assets/images/translator_pic.jpg', description: 'This is a profile description'},
+    {name: 'Maxim', image: 'assets/images/translator_pic2.jpg', description: 'This is a profile description'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
