b675b8cfb613312fc5162498b8c9512bf2821497
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-04-18 01:58:58 +0300

    added some photos and text for the popup adds

--- src/app/components/ads-modal/ads-modal.component.ts
+++ src/app/components/ads-modal/ads-modal.component.ts
@@ -12,6 +12,24 @@ export class AdsModalComponent implements OnInit, AfterViewInit {
   isModalShown = false;
   shownOnce = false;
 
+  ads = [
+    {
+      id: 1,
+      image: 'assets/images/ads/ad-1.jpg',
+      content: 'Stuck writing a letter to your boss? Want to translate documents from abroad? \
+       Or maybe you want to learn a new language with the best translators in the industry? \
+       Contact us and we\'ll be happy to assist you!'
+    },
+    {
+      id: 2,
+      image: 'assets/images/ads/ad-2.jpg'
+    },
+    {
+      id: 3,
+      image: 'assets/images/ads/ad-3.jpg'
+    }
+  ];
+
   showModal(): void {
     this.isModalShown = true;
   }
@@ -31,9 +49,9 @@ export class AdsModalComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    if (!sessionStorage.getItem('popup')) {
+    // if (!sessionStorage.getItem('popup')) {
       this.showModal();
-    }
+    // }
   }
 
 
