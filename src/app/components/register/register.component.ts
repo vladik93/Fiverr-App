a906686f1e9af726fa5d1534a9da0743204259dc
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-05-01 19:47:54 +0300

    creating a regex validator for usernames to exclude special chas

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -25,9 +25,9 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      username: new FormControl('', [ Validators.required, Validators.minLength(3)]),
+      username: new FormControl('', [ Validators.required, Validators.minLength(3), Validators.pattern('[^A-Za-z0-9 _]+')]),
       email: new FormControl('', [ Validators.required, Validators.email ]),
-      password: new FormControl('', [ Validators.required ]),
+      password: new FormControl('', [ Validators.required, Validators.minLength(6)]),
       password_confirm: new FormControl('', [ Validators.required]),
       recieve_emails: new FormControl(false)
     }, { validators: passwordConfirming});
@@ -57,6 +57,7 @@ export class RegisterComponent implements OnInit {
       );
     } else {
       console.log('Form is invalid');
+      console.log(this.registerForm);
     }
   }
 
