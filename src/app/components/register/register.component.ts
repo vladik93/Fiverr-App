456a14f461416f30c3536e8eb5a3c959a16c0641
Vlad Semyonov <vladik.semyonov@gmail.com>
2020-05-01 21:17:57 +0300

    unsaved change

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -25,7 +25,7 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      username: new FormControl('', [ Validators.required, Validators.minLength(3), Validators.pattern('[^A-Za-z0-9 _]+')]),
+      username: new FormControl('', [ Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [ Validators.required, Validators.email ]),
       password: new FormControl('', [ Validators.required, Validators.minLength(6)]),
       password_confirm: new FormControl('', [ Validators.required]),
